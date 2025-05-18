49abd3161f1d051db5333da9f8a8c204f169ffc4
shrooq-notchARC <shrooq.alamoudi@notch.sa>
2025-05-18 11:13:17 +0300

    Delete check_excel.py

--- check_excel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the Excel file
-file_path = "/Users/shurooqalamoudi/Documents/Notch/Projects_Devices_Project/devices.xlsx"
-df = pd.read_excel(file_path)
-
-# Print the first 10 rows of the 'Device Name' column
-print(df[["Device Name"]].head(10))
-
